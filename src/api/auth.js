e8ebcafbe35e7023bc0470c5539e142c6fdb2614
rjimenez <reinaldo.jimenez.montero@gmail.com>
2023-10-13 18:16:40 -0400

    firs commit

---
+++ src/api/auth.js
@@ -0,0 +1,13 @@
+import axios from './axios';
+
+// const API = 'http://localhost:3001/api/v1'
+
+export const loginRequest = user => axios.post(`/auth/signin`, user);
+
+export const logoutRequest = () => axios.post(`/auth/logout`);
+
+export const verifyTokenRequest = () => axios.get(`/auth/whoami`);
+
+export const generarQRRequest = data => axios.post(`/users/generarQR`, data);
+
+export const getVersion = () => axios.get(`/prueba`);
